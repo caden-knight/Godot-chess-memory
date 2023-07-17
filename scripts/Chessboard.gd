72b112abc61410c1929a73ec7b413a6d3487b6a3
Caden Morrison <64432691+caden-knight@users.noreply.github.com>
2023-07-16 21:26:00 -0600

    fixed a bug that crashed the game if the player finished all target squares

--- scripts/Chessboard.gd
+++ scripts/Chessboard.gd
@@ -6,15 +6,17 @@ extends Node2D
 @onready var target_square_label := $TargetSquare
 @onready var guess_square_label := $GuessSquare
 @onready var target_square: String = Singleton.target_squares.pick_random()
+@onready var original_screen_width: float = viewport_size.x
+@onready var original_screen_height: float = viewport_size.y
 
 var isWhite: bool = true
-var squareX: float = 0.0
-var squareY: float = 0.0
+var squareX: float = 0
+var squareY: float = 0
 var direction: int = 1
 var coords = Singleton.coords
 
 func _ready():
-	setup_board()
+	setup_board(false, viewport_size.y)
 	Singleton.target_square = target_square
 
 # update labels when mouse is clicked
@@ -28,8 +30,11 @@ func _input(event):
 		var labelColor: Color = Color(0, 255, 0, 1) if Singleton.isCorrect else Color(255, 0, 0, 1)
 		guess_square_label.add_theme_color_override("font_color", labelColor )
 
-# sets up all 64 squares and aligns them to make a chessboard
-func setup_board() -> void:
+# sets up the 8x8 grid and aligns the squares to make a chessboard
+func setup_board(show_square_labels: bool, square_size: float) -> void:
+	
+	print(square_size)
+	
 	# necessary for the order the squares are instantiated
 	coords[0].reverse()
 	
@@ -37,8 +42,8 @@ func setup_board() -> void:
 	target_square_label.text = target_square
 	
 	# allows the board to fill the screen from top to bottom perfectly
-	var square_size: Vector2 = Vector2(viewport_size.y / 8, viewport_size.y / 8,)
-	var offset := viewport_size.x - (square_size.x * 8)
+	var square_dimensions: Vector2 = Vector2(square_size / 8, square_size / 8)
+	var offset := get_viewport_rect().size.x - (square_dimensions.x * 8)
 	
 	# sets up and labels the chess board
 	for file in range(7, -1, -1):
@@ -47,19 +52,20 @@ func setup_board() -> void:
 			var squareLabel := square.get_child(0)
 
 			# setup squares and add to scene
-			square.set_size(square_size)
+			square.set_size(square_dimensions)
 			square.position = Vector2(squareX, squareY)
 			square.color = Color(1,1,1,1) if isWhite else Color(0,0,0,1)
 			squareLabel.text = coords[0][file] + str(coords[1][rank])
-			squareLabel.visible = false 
+			squareLabel.visible = show_square_labels 
 			squares.add_child(square)
+			target_square_label.position = Vector2(0,0)
 
 			# space and align the squares
 			if rank != 0:
-				squareY += square_size.y
+				squareY += square_dimensions.y
 				isWhite = !isWhite
 			else:
-				squareX += square_size.x
+				squareX += square_dimensions.x
 				squareY = 0
 
 	# centers the chessboard
--- scripts/Square.gd
+++ scripts/Square.gd
@@ -12,20 +12,27 @@ func _input(event):
 			var target_square: String = Singleton.target_square
 			var target_squares : Array = Singleton.target_squares
 			var player_guess: String = $Label.text
-			
-			# update player guess in the singleton
-			Singleton.player_guess = player_guess
-			
+
 			# get out of function if player clicks a square they have already guessed
+			# or if target_square array is empty
 			if correctly_guessed_tiles.has(player_guess):
+				Singleton.isCorrect = false
 				return
 			
 			if player_guess == target_square:
+				Singleton.player_guess = player_guess # update player guess in the singleton
 				$Label.visible = true
 				Singleton.isCorrect = true
+				correctly_guessed_tiles.append(player_guess)
 				Singleton.target_squares.erase(player_guess) # get rid of correctly guessed squares
 			else:
 				Singleton.isCorrect = false
+				Singleton.player_guess = player_guess # update player guess in the singleton
+				
+				
+			if Singleton.target_squares.size() <= 0:
+				Singleton.target_square = ""
+				return
 			
 			# choose new target square
 			Singleton.target_square = Singleton.target_squares.pick_random()
