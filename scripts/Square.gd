9d80d33f01225d5644dddd05c3a41c3a213c5c5f
Caden Morrison <64432691+caden-knight@users.noreply.github.com>
2023-07-17 01:06:47 -0600

    added comments and refactored code

--- scripts/Square.gd
+++ scripts/Square.gd
@@ -7,37 +7,32 @@ var coords : Array = Singleton.coords
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		var mouse_position = get_global_mouse_position()
-		
+
 		if get_global_rect().has_point(mouse_position):
 			var target_square: String = Singleton.target_square
 			var target_squares : Array = Singleton.target_squares
 			var player_guess: String = $Label.text
 
 			# get out of function if player clicks a square they have already guessed
-			# or if target_square array is empty
 			if correctly_guessed_tiles.has(player_guess):
 				Singleton.isCorrect = false
 				return
-			
+
+			# evaluate the player guess against the target square
 			if player_guess == target_square:
 				Singleton.player_guess = player_guess # update player guess in the singleton
-				$Label.visible = true
-				Singleton.isCorrect = true
-				correctly_guessed_tiles.append(player_guess)
+				$Label.visible = true # see coordinate label on the chessboard itself
+				Singleton.isCorrect = true # sets text color to green
+				correctly_guessed_tiles.append(player_guess) # keep track of squares already clicked on
 				Singleton.target_squares.erase(player_guess) # get rid of correctly guessed squares
 			else:
-				Singleton.isCorrect = false
+				Singleton.isCorrect = false # sets text color to red
 				Singleton.player_guess = player_guess # update player guess in the singleton
-				
-				
+
+			# if target_square array is empty get out of function
 			if Singleton.target_squares.size() <= 0:
 				Singleton.target_square = ""
 				return
-			
+
 			# choose new target square
 			Singleton.target_square = Singleton.target_squares.pick_random()
-
-
-
-
-
